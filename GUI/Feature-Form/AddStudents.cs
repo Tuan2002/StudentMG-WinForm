3fbf1bc4e03b45a768ee145cd0914ad93a4d4cd8
Tran Manh <manhtran18001008@gmail.com>
2023-04-06 13:48:38 +0700

    Update

---
+++ GUI/Feature-Form/AddStudents.cs
@@ -0,0 +1,45 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.Feature_Form
+{
+    public partial class AddStudents : Form
+    {
+        public AddStudents()
+        {
+            InitializeComponent();
+        }
+        private Response getListUser()
+        {
+            DatabaseAccess access = new DatabaseAccess();
+            return access.getListUser();
+        }
+        public void loadData()
+        {
+            Response res = new Response();
+            res = getListUser();
+            if (res.code == "success")
+            {
+                dgvStudents.Rows.Clear();
+                foreach (DataRow row in res.data.Rows)
+                {
+                    dgvStudents.Rows.Add(row["studentID"].ToString(), row["fullname"].ToString(), row["adress"].ToString());
+                }
+            }
+            else
+                MessageBox.Show("Lỗi");
+        }
+        private void AddMajor_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+    }
+}
